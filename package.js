cc062680cd2d12d11683bdc8dc84c270ae3280c7
lapapadelapapa <minecrafttecnico120@gmail.com>
2026-05-16 00:28:48 -0300

    Add files via upload

---
+++ package.js
@@ -0,0 +1,13 @@
+{
+  "name": "space-survivor-mmo",
+  "version": "1.0.0",
+  "description": "Juegazo espacial multijugador multiplataforma",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.19.0",
+    "socket.io": "^4.7.0"
+  }
+}
